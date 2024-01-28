b53886c9d07a394701693caf9848f6a838a79bab
Enolyo <l.bryan@hotmail.fr>
2024-01-28 23:15:40 +0100

    correction orthographe + trad

--- src/resources/lang/fr/notifications.php
+++ src/resources/lang/fr/notifications.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'seat_buyback_new_buyback_notification_label' => 'New Buyback',
+    'seat_buyback_new_buyback_notification_label' => 'Nouveau rachat',
 
-    'new_buyback_title' => 'New Buyback',
-    'new_buyback_desc' => 'Contract Details:',
-    'new_buyback_field_value' => 'ISK Value',
-    'new_buyback_field_item_count' => 'Item Count',
+    'new_buyback_title' => 'Nouveau rachat',
+    'new_buyback_desc' => 'Détails du contrat :',
+    'new_buyback_field_value' => 'Valeur en ISK :',
+    'new_buyback_field_item_count' => 'Nombre d\'item',
     'new_buyback_field_item_value' => ':itemCount Items',
-    'new_buyback_field_contract_id' => 'Contract ID'
+    'new_buyback_field_contract_id' => 'ID du contrat'
 ];
